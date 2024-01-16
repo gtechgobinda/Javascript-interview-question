0428aa373aeef7efa9302652058c41d9114df6cf
Gobinda Saha <72671943+gtechgobinda@users.noreply.github.com>
2024-01-16 18:27:29 +0530

    new folder created

---
+++ 100 JAVASCRIPT QUESTIONS/index.js
@@ -0,0 +1,25 @@
+// difference between var , let and const 
+
+//-------using var--------- 
+
+// function example(){
+//     if(true){
+//         var count=10;
+//         console.log(count);
+//     }
+//     console.log(count);
+// }
+// example();
+
+//------using let--------
+
+// function example(){
+//     if(true){
+//         let count=10;
+//         console.log(count);
+//     }
+//     // console.log(count);
+// }
+// example()
+
+//

ebc5dea9c40595e5d5eb626ea230a1f295b49437
Gobinda Saha <72671943+gtechgobinda@users.noreply.github.com>
2024-07-01 06:56:43 +0530

    done

---
+++ klizo/js/script.js
@@ -0,0 +1,153 @@
+// data type
+// string
+
+//-------promise
+// function fetchData(){
+//     return new Promise((resolve)=>{
+//       setTimeout(()=>{
+//         resolve("Data Received")
+//       },2000)
+//     })
+//   }
+
+//   async function getData(){
+//     console.log("fetching the data");
+//     const data=await fetchData();
+//     console.log(data);
+//   }
+//   getData()
+
+//-------callback function
+// function callbackFunc(){
+//     console.log("I am Callback Func")
+// }
+
+// function HigerOrderFunc(func){
+//     console.log("I am Higher Order Function")
+//     func()
+// }
+
+// HigerOrderFunc(callbackFunc)
+
+//--------var vs let vs const--------
+
+// var functional/global scope
+// function exampleVar() {
+//   if (true) {
+//     var x = 10;
+//     console.log(x);
+//   }
+//   console.log(x);
+// }
+// exampleVar()
+
+// let block scope
+// function exampleLet(){
+//     if(true){
+//         let y=10;
+//         console.log(y)
+//     }
+//     console.log(y)
+// }
+// exampleLet()
+
+// const block scope
+// function exampleConst(){
+//     if(true){
+//         const z=10;
+//         console.log(z)
+//     }
+//     console.log(z)
+// }
+// exampleConst()
+
+// //------temporal deadzone example
+//     console.log(a) // undefined
+//     console.log(b)   //can't access before initialized
+//     console.log(c) //can't access before initialized
+
+//     var a=10;
+//     let b=20;
+//     const c=30;
+
+//     console.log(a) //10
+//     console.log(b) //20
+//     console.log(c) //30
+
+// -----setTimeOut
+
+// setTimeout(() => {
+//   console.log("Hello World");
+// }, 20000);
+
+// // -----setInterval
+// setInterval(()=>{
+//     console.log("Klizo")
+// },1000)
+
+// // ---------callback hell
+// function step1(callback) {
+//   setTimeout(() => {
+//     console.log("Step 1 completed");
+//     callback(10);
+//   }, 1000);
+// }
+// function step2(step1Result, callback) {
+//   setTimeout(() => {
+//     console.log("Step 2 completed");
+//     callback(step1Result + 10);
+//   }, 1000);
+// }
+// function step3(step2Result, callback) {
+//   setTimeout(() => {
+//     console.log("Step 2 completed");
+//     callback(step2Result + 10);
+//   }, 1000);
+// }
+
+// step1((result1) => {
+//   step2(result1,(result2) => {
+//     step3(result2,(result3)=>{
+//         console.log(result3)
+//     })
+//   });
+// });
+
+
+//----using async await (alternative way callback)
+function step1() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log("Step 1 completed");
+        resolve(10);
+      }, 1000);
+    });
+  }
+  function step2(step1Result) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log("Step 2 completed");
+        resolve(step1Result + 10);
+      }, 1000);
+    });
+  }
+  function step3(step2Result) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log("Step 2 completed");
+        resolve(step2Result + 10);
+      }, 1000);
+    });
+  }
+
+const executedAllSteps = async () => {
+    try {
+        const result1 = await step1();
+        const result2 = await step2(result1);
+        const finalResult = await step3(result2);
+        console.log('Final result:', finalResult);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
+executedAllSteps();
---
+++ klizo/question.md
@@ -0,0 +1,15 @@
+1.Why Need DOCTYPE HTML, Why This is need to write in first line?
+2.Block
+3.box shadow
+4.speceficity
+5.css all unit
+6.viewport
+7.why index.html entry point?
+8.we can't write ul inside p 
+9.table
+10.why border-box (border-box vs content-box)
+11.flexbox
+12.psudo selector 
+13.css variable
+14.vanilla javascript vs javascrpt
+
